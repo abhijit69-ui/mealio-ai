be2ee9f5eb897a9656e0cbe0f1eb7e0c49f32c06
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-04-26 13:06:17 +0530

    feat: add category management features with create, update, and delete functionality; integrate react-hook-form for form handling and zod for validation

--- src/app/(dashboard)/admin/categories/_services/categoryMutation.ts
+++ src/app/(dashboard)/admin/categories/_services/categoryMutation.ts
@@ -1,10 +1,36 @@
 "use server";
 
 import db from "@/lib/db";
-import { executeAction } from "@/lib/types/executeAction";
+import { executeAction } from "@/lib/executeAction";
+import { CategorySchema } from "../_types/categorySchema";
 
 export const deleteCategory = async (id: number) => {
   await executeAction({
     actionFn: () => db.category.delete({ where: { id } }),
   });
+};
+
+export const createCategory = async (data: CategorySchema) => {
+  await executeAction({
+    actionFn: () =>
+      db.category.create({
+        data: {
+          name: data.name,
+        },
+      }),
+  });
+};
+
+export const updateCategory = async (data: CategorySchema) => {
+  if (data.action === "update") {
+    await executeAction({
+      actionFn: () =>
+        db.category.update({
+          where: { id: data.id },
+          data: {
+            name: data.name,
+          },
+        }),
+    });
+  }
 };
--- src/app/(dashboard)/admin/categories/_services/useCategoryMutation.ts
+++ src/app/(dashboard)/admin/categories/_services/useCategoryMutation.ts
@@ -1,8 +1,13 @@
 "use client";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteCategory } from "./categoryMutation";
+import {
+  createCategory,
+  deleteCategory,
+  updateCategory,
+} from "./categoryMutation";
 import { toast } from "sonner";
+import { CategorySchema } from "../_types/categorySchema";
 
 export const useDeleteCategory = () => {
   const queryClient = useQueryClient();
@@ -13,6 +18,34 @@ export const useDeleteCategory = () => {
     onSuccess: () => {
       // TODO: show user a toast
       toast.success("Category deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+};
+
+export const useCreateCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: CategorySchema) => {
+      await createCategory(data);
+    },
+    onSuccess: () => {
+      toast.success("Category created successfully");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+  });
+};
+
+export const useUpdateCategory = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: CategorySchema) => {
+      await updateCategory(data);
+    },
+    onSuccess: () => {
+      toast.success("Category updated successfully");
       queryClient.invalidateQueries({ queryKey: ["categories"] });
     },
   });
